71de70a27d050c90b5c609612afcea692f555150
Anatoly <anatolyshangin@mail.ru>
2023-01-06 09:05:02 +0300

    initial commit, begin app

---
+++ bboard/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from .models import Bb
+# Create your views here.
+
+
+def index(request):
+    bbs = Bb.objects.all()
+    return render(request, 'bboard/index.html', {'bbs': bbs})

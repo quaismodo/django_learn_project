e732d51f33cdcba16390a74ca85b36b4a7fd5584
Anatoly <anatolyshangin@mail.ru>
2023-01-08 01:59:20 +0300

    ending app

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, by_rubric, BbCreateView
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('add/', BbCreateView.as_view(), name='add'),
 ]
